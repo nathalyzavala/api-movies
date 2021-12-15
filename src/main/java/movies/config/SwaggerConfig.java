9772f170cde7de44a6f2f9ebc233f8dcb178192c
nathalyzavala <24iveth.ramirez@gmail.com>
2021-12-14 23:27:32 -0300

    Proyecto

--- src/main/java/movies/config/SwaggerConfig.java
+++ src/main/java/movies/config/SwaggerConfig.java
@@ -17,7 +17,7 @@ public class SwaggerConfig {
 	public Docket api() {
 		return new Docket(DocumentationType.SWAGGER_2)
 				.select()
-				.apis(RequestHandlerSelectors.basePackage("tech.istrategies.movies.controllers"))
+				.apis(RequestHandlerSelectors.basePackage("movies.controllers"))
 				.paths(PathSelectors.any())
 				.build();
 	}
